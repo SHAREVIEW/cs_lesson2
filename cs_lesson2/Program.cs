3cad70349e70c7feccc758799e7e3ed0384b82d7
david <2657070472@qq.com>
2018-10-26 09:50:21 +0800

    变量

--- cs_lesson2/Program.cs
+++ cs_lesson2/Program.cs
@@ -10,6 +10,12 @@ namespace cs_lesson2
     {
         static void Main(string[] args)
         {
+            int myInteger;      //声明变量
+            string myString;
+            myInteger = 17;          //赋值
+            myString = "\"myInteger\" is ";         
+            Console.WriteLine("{0}{1}.", myString, myInteger);   //输出
+            Console.ReadKey();
         }
     }
 }
